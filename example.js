8951340ddf6a946e51def9e38db7b7e473f91c22
suvro404 <debnathsuvro@gmail.com>
2022-07-05 16:27:27 +0600

    primarily added count animation

--- example.js
+++ example.js
@@ -2,8 +2,11 @@ let addBtn = document.getElementById('add-btn')
 let removeBtn = document.getElementById('remove-btn')
 let count = document.getElementById('count')
 
+let animCheck = document.getElementById('animation-checkbox')
+
 function add() {
   count.innerHTML = Number(count.innerHTML) + 1
+  console.log("animation : ", animCheck.checked)
 }
 
 function remove() {
