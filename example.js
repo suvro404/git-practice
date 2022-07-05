db1449e6dc95f09017c7aa1429e72a3906df4d1f
Suvro Debnath <debnathsuvro@gmail.com>
2022-07-05 17:11:55 +0600

    Develop (#3)
    * included remove functionality
    
    * Feature/count animation (#2)
    
    * primarily added count animation
    
    * added font size animation
    
    * added font color animation
    
    * removed console message

--- example.js
+++ example.js
@@ -2,12 +2,36 @@ let addBtn = document.getElementById('add-btn')
 let removeBtn = document.getElementById('remove-btn')
 let count = document.getElementById('count')
 
+let animCheck = document.getElementById('animation-checkbox')
+
 function add() {
   count.innerHTML = Number(count.innerHTML) + 1
+  animate()
 }
 
 function remove() {
   count.innerHTML = Number(count.innerHTML) - 1
+  animate()
+}
+
+function animate() {
+  if(animCheck.checked) {
+    count.style.fontSize = 'x-small'
+    let animTimeout = setTimeout(function() {
+      if(animTimeout) {
+        clearTimeout(animTimeout)
+      }
+      count.style.fontSize = 'x-large'
+      let countNum = Number(count.innerHTML)
+      if(countNum > 0) {
+        count.style.color = "green"
+      } else if (countNum < 0) {
+        count.style.color = "red"
+      } else {
+        count.style.color = "black"
+      }
+    }, 300)
+  }
 }
 
 addBtn.addEventListener("click", add)
