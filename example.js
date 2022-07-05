5390fc0e1bf89c04e39f925cd762fa4795030e29
suvro404 <debnathsuvro@gmail.com>
2022-07-05 15:39:15 +0600

    included remove functionality

--- example.js
+++ example.js
@@ -1,8 +1,14 @@
-let btn = document.getElementById('count-btn')
+let addBtn = document.getElementById('add-btn')
+let removeBtn = document.getElementById('remove-btn')
 let count = document.getElementById('count')
 
 function add() {
   count.innerHTML = Number(count.innerHTML) + 1
 }
 
-btn.addEventListener("click", add)
+function remove() {
+  count.innerHTML = Number(count.innerHTML) - 1
+}
+
+addBtn.addEventListener("click", add)
+removeBtn.addEventListener("click", remove)
